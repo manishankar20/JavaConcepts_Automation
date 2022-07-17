328b1d008382e41c6a7e6fd10debefd9e4112c9b
maniAllwyn <mani@allwyncorp.com>
2022-07-17 18:16:23 +0530

    Anagram

--- JavaConcepts/src/BasicPrograms/StringAnagram.java
+++ JavaConcepts/src/BasicPrograms/StringAnagram.java
@@ -1,9 +1,28 @@
 package BasicPrograms;
 
+import java.util.Arrays;
+
 public class StringAnagram {
 
 	public static void main(String[] args) {
-		System.out.println("Mani -");
+		anagram("Army", "marry");
+	}
+
+	public static void anagram(String s1, String s2) {
+		if (s1.length() == s2.length()) {
+			char ch1[] = s1.toLowerCase().toCharArray();
+			char ch2[] = s2.toLowerCase().toCharArray();
+			Arrays.sort(ch1);
+			Arrays.sort(ch2);
+			String s = String.valueOf(ch2);
+			if (Arrays.equals(ch1, ch2)) {
+				System.out.println("Given strings are anagram");
+			} else {
+				System.out.println("Given strings are not anagram");
+			}
+		} else {
+			System.out.println("Given strings are not anagram");
+		}
+
 	}
 }
- 
